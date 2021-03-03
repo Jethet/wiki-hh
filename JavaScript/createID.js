1f2a88c93f499ad056f92326859e4d512da3b837
Jethet <henriette.hettinga@protonmail.com>
2021-03-03 17:52:35 +0100

    Add examples of randomise and index creation

---
+++ JavaScript/createID.js
@@ -0,0 +1,7 @@
+// Create id's for items with JavaScript:
+
+if (data.items.length > 0) {
+  ID = data.items[data.items.length - 1].id + 1;
+} else {
+  ID = 0;
+}
---
+++ JavaScript/randomise.js
@@ -0,0 +1,23 @@
+// Randomize properties, example: random color from an array of colors
+
+let colors = [
+  "#aed6f1",
+  "#76d7c4",
+  "#f7dc6f",
+  "#f8c471",
+  "#e74c3c",
+  "#f1c40f",
+  "#c39bd3",
+  "#a569bd",
+  "#45b39d",
+  "#28b463",
+  "#eb984e",
+  "#27ae60",
+  "#05E3EE",
+  "#3498db",
+  "#dc7633"
+]
+
+this.color = colors[Math.floor(Math.random() * colors.length)]
+
+
