95ac63ea24c0f9e1e02b70484482b7d74afa135b
Jethet <henriette.hettinga@protonmail.com>
2021-03-03 17:28:00 +0100

    Add js algorithms; add CSS snippet background

---
+++ JavaScript/algorithms.js
@@ -0,0 +1,27 @@
+// Sort an array of numbers
+function sortNumbers(arr) {
+  // Sort numbers from least to greatest:
+  let sortArr = arr.sort(function(a, b){return a-b})
+  return sortArr;
+}
+
+// Falsy values in JavaScript are false, null, 0, "", undefined, and NaN. Check these with:
+if (value) {
+  // do something
+  }
+
+// Dividing arrays: the Array instance splice() method:
+const list = [1, 2, 3, 4, 5, 6]
+const half = Math.ceil(list.length / 2);
+
+const firstHalf = list.splice(0, half)
+const secondHalf = list.splice(-half)
+
+
+//Remove LAST char of string: (slice does not alter original array, create new)
+const text = 'abcdef'
+const editedText = text.slice(0, -1) //'abcde'
+
+//Remove FIRST char of string:
+const text = 'abcdef'
+const editedText = text.slice(1) //'bcdef'
