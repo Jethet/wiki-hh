b2ed31886018452c47c2c5f8aeb769b1e96f3531
Jethet <henriette.hettinga@protonmail.com>
2021-06-04 16:17:22 +0200

    Add code snippet on generating next id in sequence

--- JavaScript/createID.js
+++ JavaScript/createID.js
@@ -5,3 +5,15 @@ if (data.items.length > 0) {
 } else {
   ID = 0;
 }
+
+
+// Alternative:
+// returns the next ID in the sequence
+function getNextAlbumId() {
+  // find the current highest ID ( we can assume the last one is the highest)
+  const highestId = albumsData[albumsData.length-1].albumId
+  // add 1 to it (we need to convert it to a number first - try it out!)
+  const nextId = parseInt(highestId) + 1
+  // return it as a string
+  return nextId.toString()
+}
